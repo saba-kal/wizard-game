8a6c0e0947a0676309ab9dd617d54c6fdbf6e80a
John Muller <johnmuller575@gmail.com>
2024-02-04 16:27:23 -0500

    added helpful fire

--- Scripts/SpellCasting/SpellEffects/Fire/Flammable.gd
+++ Scripts/SpellCasting/SpellEffects/Fire/Flammable.gd
@@ -5,23 +5,43 @@ var duration: float = 5
 var time_since_fire: float = 0
 var fire_started: bool = false
 var fire_source: Node3D
+var un_duration: float = 5
+var time_since_un_fire: float = 0
+var un_fire_started: bool = false
+var un_fire_source: Node3D
 
 
 func _process(delta):
-    if !self.fire_started:
-        return
+    if self.fire_started:
+        process_fire(delta)
+    if self.un_fire_started:
+        process_un_fire(delta)
+
+func process_fire(delta):
     self.time_since_fire += delta
     if self.time_since_fire >= self.duration:
         self.extinguish()
         self.time_since_fire = 0
         self.fire_started = false
 
+func process_un_fire(delta):
+    self.time_since_un_fire += delta
+    if self.time_since_un_fire >= self.un_duration:
+        self.un_extinguish()
+        self.time_since_un_fire = 0
+        self.un_fire_started = false
 
 func start_fire(fire_duration: float, source: Node3D):
     self.fire_started = true
     self.duration = fire_duration
     self.fire_source = source
     self.light()
+
+func start_un_fire(fire_duration: float, source: Node3D):
+    self.un_fire_started = true
+    self.un_duration = fire_duration
+    self.un_fire_source = source
+    self.un_light()
 
 
 func light():
@@ -30,3 +50,9 @@ func light():
 
 func extinguish():
     pass # implmented by inherited classes
+
+func un_light():
+    pass
+
+func un_extinguish():
+    pass
---
+++ Scripts/SpellCasting/SpellEffects/Fire/UnIgniteEnemiesInArea.gd
@@ -0,0 +1,13 @@
+extends Area3D
+
+@export var duration: float = 60
+
+
+func _ready():
+    self.body_entered.connect(self.on_body_entered)
+
+
+func on_body_entered(body: Node3D):
+    var flammable_nodes: Array = Util.get_child_nodes_of_type(body, Flammable)
+    for flammable_node in flammable_nodes:
+        flammable_node.start_un_fire(self.duration, self)
