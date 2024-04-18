7ac7132524598daedb2a029a18920f789167627a
Clampitar <clampitar@gmail.com>
2024-04-18 15:32:00 -0400

    realistic ice floating

---
+++ Scripts/Environment/Common/Floating.gd
@@ -0,0 +1,20 @@
+class_name Floating extends Node3D
+
+@export var force: float = 3.5
+var parent: RigidBody3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    var parent = get_parent_node_3d()
+    if(parent is RigidBody3D):
+        self.parent = parent
+    else:
+        print("error: Floating must have a RigidBody parent")
+        queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    var points = Util.get_child_nodes_of_type(self, RayCast3D)
+    for point: RayCast3D in points:
+        if(point.is_colliding()):
+            parent.apply_force(Vector3(0, force, 0), point.global_position - parent.global_position)
