f67685b698ef25ea978ec225beab1e374fe039f8
Clampitar <clampitar@gmail.com>
2024-11-03 14:35:30 -0500

    Imported models

--- Scripts/Environment/InteractiveLevelElements/BouncePlatform.gd
+++ Scripts/Environment/InteractiveLevelElements/BouncePlatform.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
 @export var bounce_amount: float = 13
+@onready var bounce_sound = $"../BounceSound"
+@onready var animation_player = $"../Springshroom/AnimationPlayer"
 
 
 func _ready():
@@ -11,3 +13,5 @@ func on_body_entered(body: Node3D):
     if body.is_in_group("Player"):
         var movement: PlayerMovement = Util.get_child_node_of_type(body, PlayerMovement)
         movement.apply_vertical_velocity(self.bounce_amount)
+        bounce_sound.play()
+        animation_player.play("Springshroom_Skeleton_002Action")
